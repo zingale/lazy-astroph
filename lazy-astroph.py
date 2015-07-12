cb0ab7ed1c925a6190ffcf1ee5d0be15e720a499
Michael Zingale <michael.zingale@stonybrook.edu>
2015-07-12 15:03:35 -0400

    make it classy

--- lazy-astroph.py
+++ lazy-astroph.py
@@ -27,46 +27,52 @@ class Papers(object):
         else:
             return cmp(len(self.keywords), len(other.keywords))
 
-def get_cat_query():
 
-    subcat = ["GA", "CO", "EP", "HE", "IM", "SR"]
-    cat_query = "%28" # open parenthesis
-    for n, s in enumerate(subcat):
-        cat_query += "astro-ph.{}".format(s)
-        if n < len(subcat)-1:
-            cat_query += "+OR+"
-        else:
-            cat_query += "%29"  # close parenthesis
+class AstrophQuery(object):
 
-    return cat_query
+    def __init__(self, start_date, end_date):
+        self.start_date = start_date
+        self.end_date = end_date
 
+        self.base_url = "http://export.arxiv.org/api/query?"
+        self.sort_query = "max_results=1000&sortBy=submittedDate&sortOrder=descending"
 
-def get_range_query():
+        self.subcat = ["GA", "CO", "EP", "HE", "IM", "SR"]
+        
+    def get_cat_query(self):
+
+        cat_query = "%28" # open parenthesis
+        for n, s in enumerate(self.subcat):
+            cat_query += "astro-ph.{}".format(s)
+            if n < len(self.subcat)-1:
+                cat_query += "+OR+"
+            else:
+                cat_query += "%29"  # close parenthesis
+
+        return cat_query
+
+    def get_range_query(self):
+        range_str = "[{}2000+TO+{}2000]".format(self.start_date.strftime("%Y%m%d"), self.end_date.strftime("%Y%m%d"))
+        range_query = "lastUpdatedDate:{}".format(range_str)
+        return range_query
+
+    def get_url(self):
+        cat_query = self.get_cat_query()
+        range_query = self.get_range_query()
+
+        full_query = "search_query={}+AND+{}&{}".format(cat_query, range_query, self.sort_query)
+
+        return self.base_url + full_query
+
+def doit():
 
     today = dt.date.today()
     oneday = dt.timedelta(days=1)
 
     today -= 4*oneday
     
-    range_str = "[{}2000+TO+{}2000]".format((today-oneday).strftime("%Y%m%d"), today.strftime("%Y%m%d"))
-
-    range_query = "lastUpdatedDate:{}".format(range_str)    
-    return range_query
-
-
-def doit():
-
-    base_url = "http://export.arxiv.org/api/query?"
-
-    cat_query = get_cat_query()
-    range_query = get_range_query()
-    sort_query = "max_results=1000&sortBy=submittedDate&sortOrder=descending"
-
-    full_query = "search_query={}+AND+{}&{}".format(cat_query, range_query, sort_query)
-
-    url = base_url + full_query
-
-    print(url)
+    q = AstrophQuery(today - oneday, today)
+    print(q.get_url())
 
 if __name__ == "__main__":
     doit()
